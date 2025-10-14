b606609815b4479212e3b23a78210fc24ecc89d5
Umang Saini <sainiumang505@gmail.com>
2025-10-14 23:32:37 +0530

    Initial clean commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Header = () => {
+    return (
+        <div className='flex flex-col justify-center items-center text-center my-20'>
+            <div className='text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500'>
+                <p>Best Text to Image Converter</p>
+                <img src={assets.star_icon}></img>
+            </div>
+            <h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-12 text-center'>Turn Text to <span className='text-blue-500'>image</span>,in seconds.</h1>
+            <p className='text-center text-lg max-w-xl mx-auto mt-5'>
+                Unleash your creativity with AI. Turn your imagination into visuals art in sseconds - just type, and watch the magic happen.
+            </p>
+            <button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-3 flex items-center gap-2 rounded-full'>
+                Generate Images
+                <img className='h-6' src={assets.star_group} />
+            </button>
+
+
+            <div className='flex flex-wrap justify-center mt-16 gap-3'>
+
+                {Array(6).fill('').map((item, index) => (
+                    <img className='rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10' src={index % 2 == 0 ? assets.sample_img_2 : assets.sample_img_1} alt="" key={index} width={70} />
+                ))}
+            </div>
+
+            <p className='mt-2 text-neutral-500'>Generated images from Imagify</p>
+        </div>
+    )
+}
+
+export default Header
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import { useContext } from 'react'
+import { assets } from '../assets/assets'
+import { Link, useNavigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+
+const Navbar = () => {
+
+    const { user } = useContext(AppContext)
+
+    const navigate = useNavigate()
+    return (
+        <div className='flex items-center justify-between py-4'>
+            <Link to='/'><img src={assets.logo} alt='logo' className='w-28 sm:w-32 lg:w-40 cursor-pointer' />
+            </Link>
+
+            <div >
+                {user ?
+                    <div className='flex items-center gap-2 sm:gap-3'>
+                        <button onClick={() => navigate('/buy')} className='flex text-center gap-2 sm:gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700'>
+                            <img className='w-5' src={assets.credit_star} alt="credit-left" />
+                            <p className='text-xs sm:text-sm font-medium text-gray-600'>Credit Left : 50</p>
+                        </button>
+                        <p className='text-gray-600 max-sm:hidden pl-4'>Hi,User</p>
+                        <div className='relative group'>
+                            <img src={assets.profile_icon} className='w-10 drop-shadow' alt='profile' />
+                            <div className='absolute hidden group-hover:block top-0 right-0 text-black rounded pt-12'>
+                                <ul className='list-none m-0 p-2 rounded-md border text-sm  '>
+                                    <li className='py-1 px-4 text-center cursor-pointer'>
+                                        LogOut
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div> :
+                    <div className='flex items-center gap-2 sm:gap-5 lg:gap-9'>
+                        <p onClick={() => navigate('/buy')} className='cursor-pointer'>Pricing</p>
+                        <button className='bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full '>LogIn</button>
+                    </div>}
+            </div>
+        </div >
+    )
+}
+
+export default Navbar
