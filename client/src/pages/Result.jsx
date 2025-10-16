bbfca7660079318bfd2579df161a4588b734b005
Umang Saini <sainiumang505@gmail.com>
2025-10-16 09:32:22 +0530

    result page

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Result = () => {
     return (
         <div>
-            Result Page
+            <div className='relative'>
+                <img src={assets.sample_img_1} alt="alien" className='max-w-sm rounded' />
+                <span className='absolute bottom-0 left-0 bg-blue-500 w-full transition-all h-1 duration-[10]' />
+            </div>
         </div>
     )
 }
