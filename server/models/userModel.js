38f087d5e12c2acb655a36f99e68c363cb36ab33
Umang Saini <sainiumang505@gmail.com>
2025-10-17 20:01:10 +0530

    server

---
+++ server/models/userModel.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [6, "Password must be at least 6 characters long"]
+    },
+    creditBalance: {
+        type: Number,
+        default: 5
+    }
+})
+
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default userModel
